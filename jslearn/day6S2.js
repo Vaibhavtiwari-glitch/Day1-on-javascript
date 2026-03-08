92ec13e7aa8b2d5e8235aeee7b4cfc00b2748a9c
Vaibhav Tiwari <vaibhavtiwari69691@gmail.com>
2026-03-08 08:06:55 +0000

    switch case

---
+++ jslearn/day6S2.js
@@ -0,0 +1,16 @@
+// node jslearn/day6S2.js
+// switch case statement
+
+const month = 5;
+switch (month){
+case 1 : console.log('jan')
+break;
+case 2 : console.log('feb')
+break;
+case 3 : console.log('march')
+break;
+case 4 : console.log('April')
+break;
+default : console.log('sahi se use kar')
+break;
+}
