ec43d2252c2a7e0899cfcb1b3012dace5046a06a
Vaibhav Tiwari <vaibhavtiwari69691@gmail.com>
2026-03-10 19:20:49 +0000

    for each and some objects inside array related things (databases)

---
+++ jslearn/day9S1.js
@@ -0,0 +1,33 @@
+// node jslearn/day9S1.js
+// for each loop
+ const coding = ['javascript','java','python'];
+// coding.forEach((e)=>{
+//     console.log(e);
+// })
+// function printme(item){
+//     console.log(item);
+// }
+// coding.forEach((item , index, arr)=>{
+//     console.log(item, index, arr);
+// });
+const mycoding = [
+    {
+        language : 'javascript',
+        extension : 'js'
+    },
+    {
+        language : 'python',
+        extension : 'py'
+    },
+    {
+        language : 'java',
+        extension : 'java'
+    },
+    {
+        language : 'ruby',
+        extension : 'ruby'
+    }
+]
+mycoding.forEach((items)=>{
+    console.log(items.language);
+})
