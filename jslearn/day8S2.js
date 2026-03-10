39f9f3afafa2279f1d3aa2a233782ad75a533a6a
Vaibhav Tiwari <vaibhavtiwari69691@gmail.com>
2026-03-10 17:46:45 +0000

    while and do while loop

---
+++ jslearn/day8S2.js
@@ -0,0 +1,18 @@
+// node jslearn/day8S2.js
+//let i = 1;
+//while(i<=30){
+//console.log(i);
+//i = i + 2;
+//}
+// let myarr = ['black', 'white', 'orange', 'blue'];
+// let i = 0;
+// while(i<myarr.length){
+//     console.log(`Array index ${i} : ${myarr[i]}`);
+//     i = i +1;
+// };
+let i = 0 ;
+do{
+    console.log(i);
+    i++;
+}
+while(i==-1);
